2fc6f4f13ce9e8e025b6010a35839b9ce0776628
nanakyeong <dlskrud468@naver.com>
2025-05-23 15:23:35 +0900

    feat : 전체적인 디자인 정리 및 내용 정리

--- js/meteor.js
+++ js/meteor.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector(".meteor-container");
+    const totalHeight = document.body.scrollHeight;
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 40; i++) {
         const meteor = document.createElement("div");
         meteor.classList.add("meteor");
 
         const delay = Math.random() * 10;
         const left = Math.random() * 100;
+        const fallDistance = 300 + Math.random() * 800;
 
         meteor.style.left = `${left}vw`;
+        meteor.style.top = `0px`;
+        meteor.style.setProperty("--fall-distance", `${fallDistance}px`);
         meteor.style.animationDelay = `${delay}s`;
         meteor.style.animationDuration = `${2 + Math.random() * 3}s`;
 
